a7cc6ac4ad8c9bd1b4bfbbf46f4210b97378f421
arvin-tan <arvin_tan@pervasivedisplays.com>
2025-02-11 10:06:48 +0800

    Update fastImageData.h

--- examples/Demo_NEW/fastupdate_src/fastImageData.h
+++ examples/Demo_NEW/fastupdate_src/fastImageData.h
@@ -12,8 +12,8 @@
 #define FastPic_2_s         (uint8_t *)&Image_970_Slavefm_02
 #else
 #define frameSize	46080
-#define Masterfm1        (uint8_t *)&Image_1200_Masterfm_01
-#define Masterfm2        (uint8_t *)&Image_1200_Masterfm_02
-#define Slavefm1         (uint8_t *)&Image_1200_Slavefm_01
-#define Slavefm2         (uint8_t *)&Image_1200_Slavefm_02
+#define FastPic_1_m        (uint8_t *)&Image_1200_Masterfm_01
+#define FastPic_2_m        (uint8_t *)&Image_1200_Masterfm_02
+#define FastPic_1_s         (uint8_t *)&Image_1200_Slavefm_01
+#define FastPic_2_s         (uint8_t *)&Image_1200_Slavefm_02
 #endif
